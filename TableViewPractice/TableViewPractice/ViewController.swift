025b3bac560b5b44964d132c8e4df6a207eebac7
Elena <elenafis@me.com>
2019-11-22 09:01:15 -0800

    f

---
+++ TableViewPractice/TableViewPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewPractice
+//
+//  Created by Elena on 11/22/19.
+//  Copyright © 2019 Elena Fis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
